3874b232302f078a0c766b4be25cd72da19d743b
shashi <shashikanth.gs@outlook.com>
2024-08-22 15:27:46 +0530

    Initial Changes

---
+++ ocp-test/src/main/java/alldev/io/ocptest/OcpTestApplication.java
@@ -0,0 +1,13 @@
+package alldev.io.ocptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcpTestApplication.class, args);
+    }
+
+}
---
+++ ocp-test/src/test/java/alldev/io/ocptest/OcpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package alldev.io.ocptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcpTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
